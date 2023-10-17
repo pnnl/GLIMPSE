1f35be8f56eeec596369f2453c86d2529c04cc6e
Armando Mendoza Sanchez <mendozasanchez166@pnnl.gov>
2023-10-17 11:14:58 -0700

    Made entire web app to desktop app with electron

--- vis_n_react/backend/gridtest.js
+++
@@ -1,7 +0,0 @@
-const grid = require("./data/123-bus-model/grid.json")
-const microGrids = grid.microgrid;
-
-for (const microgrid of microGrids)
-{
-   console.log(microgrid);
-}
--- vis_n_react/client/src/App/config/socket.js
+++
@@ -1,7 +0,0 @@
-import { io } from 'socket.io-client';
-
-const URL = process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:8000';
-
-export const socket = io(URL, {
-   autoConnect: false
-});
