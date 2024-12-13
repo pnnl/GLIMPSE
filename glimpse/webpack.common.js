c29390dab61da2f5d2320179f223524053d56f48
Mendoza Sanchez <mendozasanchez166@pnnl.gov>
2024-12-12 17:04:29 -0800

    set mode to development

--- glimpse/webpack.common.js
+++ glimpse/webpack.common.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 module.exports = {
-   mode: "production",
+   mode: "development",
    entry: "./renderer/src/index.js",
    devtool: "inline-source-map",
    target: "electron-renderer",
