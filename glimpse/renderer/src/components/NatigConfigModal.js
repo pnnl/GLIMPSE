9f7839a24d8be4e7627be69ecc942568987bb0d9
Mendoza Sanchez <mendozasanchez166@pnnl.gov>
2025-01-14 12:00:32 -0800

    Added almost all DDoS Natig Settings

--- glimpse/renderer/src/components/NatigConfigModal.js
+++ glimpse/renderer/src/components/NatigConfigModal.js
@@ -1,21 +1,31 @@
 import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 import {
+   Accordion,
+   AccordionSummary,
    Dialog,
    DialogActions,
    DialogContent,
    DialogTitle,
+   FormControl,
    InputAdornment,
+   InputLabel,
+   MenuItem,
+   Select,
    Stack,
    TextField,
    Tooltip,
 } from "@mui/material";
+import { ExpandMore } from "@mui/icons-material";
 import { CustomButton, CustomFormControlLabel, CustomSwitch } from "../utils/CustomComponents";
 import { sendNatigConfig } from "../utils/webSocketClientUtils";
 
+const packetSizeOptions = [500, 1280, 1500];
+
 const NatigConfigModal = ({ onMount }) => {
    const [openForm, setOpenForm] = useState(false);
-   const [configObj, setConfigObj] = useState({
+   const [expanded, setExpanded] = useState(false);
+   const [generalConfig, setGeneralConfig] = useState({
       SimTime: {
          label: "Simulation Time",
          desc: "Controls how long the simulation is going to run in seconds.",
@@ -47,28 +57,78 @@ const NatigConfigModal = ({ onMount }) => {
          value: 0,
       },
    });
+   const [DDosConfig, setDDosConfig] = useState({
+      Active: {
+         label: "Enable DDoS Attack",
+         desc: "Controls whether or not the DDoS attack will be active for the simulation",
+         value: false,
+      },
+      threadsPerAttacker: {
+         label: "Number of Attackers",
+         desc: "Control the number of attackers",
+         value: 0,
+      },
+      Start: {
+         label: "Start",
+         desc: "Number of seconds after the start of the simulation when the attack starts",
+         value: 0,
+      },
+      End: {
+         label: "End",
+         desc: "Number of seconds after the start of the simulation when the attack stops",
+         value: 0,
+      },
+      PacketSize: {
+         label: "Packet Size",
+         desc: "Controls the maximum transmissible unit the attackers link is set to",
+         value: 500,
+      },
+   });
 
-   const handleChange = (e) => {
+   const handleGeneralConfig = (e) => {
       const { name, value, type, checked } = e.target;
 
-      setConfigObj({
-         ...configObj,
+      setGeneralConfig({
+         ...generalConfig,
          [name]: {
-            ...configObj[name],
+            ...generalConfig[name],
             value: type === "checkbox" ? checked : value,
          },
       });
    };
 
-   const send = () => {
-      const natigConfigObj = Object.entries(configObj).reduce(
+   const handleDDosConfig = (e) => {
+      const { name, value, type, checked } = e.target;
+
+      setDDosConfig({
+         ...DDosConfig,
+         [name]: {
+            ...DDosConfig[name],
+            value: type === "checkbox" ? checked : value,
+         },
+      });
+   };
+
+   const getSendObj = (formConfigObj) => {
+      return Object.entries(formConfigObj).reduce(
          (o, [key, val]) => ({ ...o, [key]: val.value }),
          {}
       );
+   };
 
-      console.log(natigConfigObj);
+   const send = () => {
+      const natigGeneralConfig = {
+         ...getSendObj(generalConfig),
+         ...getSendObj(DDosConfig),
+      };
 
-      sendNatigConfig(natigConfigObj);
+      console.log(natigGeneralConfig);
+
+      sendNatigConfig(natigGeneralConfig);
+   };
+
+   const handleExpand = (panelName) => (e, isExpanded) => {
+      setExpanded(isExpanded ? panelName : false);
    };
 
    useEffect(() => {
@@ -80,95 +140,181 @@ const NatigConfigModal = ({ onMount }) => {
          <Dialog open={openForm}>
             <DialogTitle>NATIG Configuration</DialogTitle>
             <DialogContent dividers>
-               <Stack direction={"column"} spacing={1}>
-                  <Tooltip title={configObj.SimTime.desc} placement="left" arrow>
-                     <TextField
-                        variant="outlined"
-                        value={configObj.SimTime.value}
-                        name="SimTime"
-                        label={configObj.SimTime.label}
-                        onChange={handleChange}
-                        type="number"
-                        slotProps={{
-                           input: {
-                              endAdornment: <InputAdornment position="end">Seconds</InputAdornment>,
-                           },
-                        }}
-                     />
-                  </Tooltip>
-                  <Tooltip title={configObj.PollReqFreq.desc} placement="left" arrow>
-                     <TextField
-                        variant="outlined"
-                        value={configObj.PollReqFreq.value}
-                        name="PollReqFreq"
-                        type="number"
-                        label={configObj.PollReqFreq.label}
-                        onChange={handleChange}
-                        slotProps={{
-                           input: {
-                              endAdornment: <InputAdornment position="end">ms</InputAdornment>,
-                           },
-                        }}
-                     />
-                  </Tooltip>
-                  <Tooltip title={configObj.UseDynTop.desc} placement="left" arrow>
-                     <CustomFormControlLabel
-                        control={
-                           <CustomSwitch
-                              name={"UseDynTop"}
-                              checked={configObj.UseDynTop.value}
-                              value={configObj.UseDynTop.value}
-                              size="medium"
-                              onChange={handleChange}
-                           />
-                        }
-                        label={configObj.UseDynTop.label}
-                        labelPlacement="start"
-                     />
-                  </Tooltip>
-                  <Tooltip title={configObj.MonitorPer.desc} placement="left" arrow>
-                     <CustomFormControlLabel
-                        control={
-                           <CustomSwitch
-                              name={"MonitorPer"}
-                              checked={configObj.MonitorPer.value}
-                              value={configObj.MonitorPer.value}
-                              size="medium"
-                              onChange={handleChange}
-                           />
-                        }
-                        label={configObj.MonitorPer.label}
-                        labelPlacement="start"
-                     />
-                  </Tooltip>
-                  <Tooltip title={configObj.StaticSeed.desc} placement="left" arrow>
-                     <CustomFormControlLabel
-                        control={
-                           <CustomSwitch
-                              name={"StaticSeed"}
-                              checked={configObj.StaticSeed.value}
-                              value={configObj.StaticSeed.value}
-                              size="medium"
-                              onChange={handleChange}
-                           />
-                        }
-                        label={configObj.StaticSeed.label}
-                        labelPlacement="start"
-                     />
-                  </Tooltip>
-                  {configObj.StaticSeed.value && (
-                     <Tooltip title={configObj.RandomSeed.desc} placement="left" arrow>
+               <Accordion
+                  expanded={expanded === "General Settings"}
+                  onChange={handleExpand("General Settings")}
+               >
+                  <AccordionSummary expandIcon={<ExpandMore />}>General Settings</AccordionSummary>
+                  <Stack sx={{ margin: "1rem" }} direction={"column"} spacing={1}>
+                     <Tooltip title={generalConfig.SimTime.desc} placement="left" arrow>
                         <TextField
                            variant="outlined"
-                           value={configObj.RandomSeed.value}
-                           name="RandomSeed"
+                           value={generalConfig.SimTime.value}
+                           name="SimTime"
+                           label={generalConfig.SimTime.label}
+                           onChange={handleGeneralConfig}
                            type="number"
-                           label={configObj.RandomSeed.label}
-                           onChange={handleChange}
+                           slotProps={{
+                              input: {
+                                 endAdornment: (
+                                    <InputAdornment position="end">Seconds</InputAdornment>
+                                 ),
+                              },
+                           }}
                         />
                      </Tooltip>
-                  )}
-               </Stack>
+                     <Tooltip title={generalConfig.PollReqFreq.desc} placement="left" arrow>
+                        <TextField
+                           variant="outlined"
+                           value={generalConfig.PollReqFreq.value}
+                           name="PollReqFreq"
+                           type="number"
+                           label={generalConfig.PollReqFreq.label}
+                           onChange={handleGeneralConfig}
+                           slotProps={{
+                              input: {
+                                 endAdornment: <InputAdornment position="end">ms</InputAdornment>,
+                              },
+                           }}
+                        />
+                     </Tooltip>
+                     <Tooltip title={generalConfig.UseDynTop.desc} placement="left" arrow>
+                        <CustomFormControlLabel
+                           control={
+                              <CustomSwitch
+                                 name={"UseDynTop"}
+                                 checked={generalConfig.UseDynTop.value}
+                                 value={generalConfig.UseDynTop.value}
+                                 size="medium"
+                                 onChange={handleGeneralConfig}
+                              />
+                           }
+                           label={generalConfig.UseDynTop.label}
+                           labelPlacement="start"
+                        />
+                     </Tooltip>
+                     <Tooltip title={generalConfig.MonitorPer.desc} placement="left" arrow>
+                        <CustomFormControlLabel
+                           control={
+                              <CustomSwitch
+                                 name={"MonitorPer"}
+                                 checked={generalConfig.MonitorPer.value}
+                                 value={generalConfig.MonitorPer.value}
+                                 size="medium"
+                                 onChange={handleGeneralConfig}
+                              />
+                           }
+                           label={generalConfig.MonitorPer.label}
+                           labelPlacement="start"
+                        />
+                     </Tooltip>
+                     <Tooltip title={generalConfig.StaticSeed.desc} placement="left" arrow>
+                        <CustomFormControlLabel
+                           control={
+                              <CustomSwitch
+                                 name={"StaticSeed"}
+                                 checked={generalConfig.StaticSeed.value}
+                                 value={generalConfig.StaticSeed.value}
+                                 size="medium"
+                                 onChange={handleGeneralConfig}
+                              />
+                           }
+                           label={generalConfig.StaticSeed.label}
+                           labelPlacement="start"
+                        />
+                     </Tooltip>
+                     {generalConfig.StaticSeed.value && (
+                        <Tooltip title={generalConfig.RandomSeed.desc} placement="left" arrow>
+                           <TextField
+                              variant="outlined"
+                              value={generalConfig.RandomSeed.value}
+                              name="RandomSeed"
+                              type="number"
+                              label={generalConfig.RandomSeed.label}
+                              onChange={handleGeneralConfig}
+                           />
+                        </Tooltip>
+                     )}
+                  </Stack>
+               </Accordion>
+               <Accordion
+                  expanded={expanded === "DDoS Settings"}
+                  onChange={handleExpand("DDoS Settings")}
+               >
+                  <AccordionSummary expandIcon={<ExpandMore />}>DDoS Settings</AccordionSummary>
+                  <Stack sx={{ margin: "1rem" }} direction={"column"} spacing={1}>
+                     <Tooltip title={DDosConfig.Active.desc} placement="left" arrow>
+                        <CustomFormControlLabel
+                           control={
+                              <CustomSwitch
+                                 name={"Active"}
+                                 checked={DDosConfig.Active.value}
+                                 value={DDosConfig.Active.value}
+                                 size="medium"
+                                 onChange={handleDDosConfig}
+                              />
+                           }
+                           label={DDosConfig.Active.label}
+                           labelPlacement="start"
+                        />
+                     </Tooltip>
+                     {DDosConfig.Active.value && (
+                        <>
+                           <Tooltip title={DDosConfig.Start.desc} placement="left" arrow>
+                              <TextField
+                                 variant="outlined"
+                                 value={DDosConfig.Start.value}
+                                 name="Start"
+                                 type="number"
+                                 label={DDosConfig.Start.label}
+                                 onChange={handleDDosConfig}
+                              />
+                           </Tooltip>
+                           <Tooltip title={DDosConfig.End.desc} placement="left" arrow>
+                              <TextField
+                                 variant="outlined"
+                                 value={DDosConfig.End.value}
+                                 name="End"
+                                 type="number"
+                                 label={DDosConfig.End.label}
+                                 onChange={handleDDosConfig}
+                              />
+                           </Tooltip>
+                           <Tooltip
+                              title={DDosConfig.threadsPerAttacker.desc}
+                              placement="left"
+                              arrow
+                           >
+                              <TextField
+                                 variant="outlined"
+                                 value={DDosConfig.threadsPerAttacker.value}
+                                 name="threadsPerAttacker"
+                                 type="number"
+                                 label={DDosConfig.threadsPerAttacker.label}
+                                 onChange={handleDDosConfig}
+                              />
+                           </Tooltip>
+                           <Tooltip title={DDosConfig.PacketSize.desc} placement="left" arrow>
+                              <FormControl>
+                                 <InputLabel>{DDosConfig.PacketSize.label}</InputLabel>
+                                 <Select
+                                    label={DDosConfig.PacketSize.label}
+                                    name={"PacketSize"}
+                                    value={DDosConfig.PacketSize.value}
+                                    onChange={handleDDosConfig}
+                                 >
+                                    {packetSizeOptions.map((size, index) => (
+                                       <MenuItem key={index} value={size}>
+                                          {`${size} bytes`}
+                                       </MenuItem>
+                                    ))}
+                                 </Select>
+                              </FormControl>
+                           </Tooltip>
+                        </>
+                     )}
+                  </Stack>
+               </Accordion>
             </DialogContent>
             <DialogActions>
                <CustomButton onClick={send}>Send</CustomButton>
